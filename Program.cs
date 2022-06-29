46e40bb7036b93a73e0bbbc904db0132e35e8b95
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-06-29 19:30:34 -0300

    add sem cor no refrão

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿Console.Writeline ("Bitch de Moscow");
+Console.Writeline ("Gotta let it go");
+Console.Writeline ("Carinha de santo, coração é de gelo");
+Console.Writeline ("Ela até tentou, bitch me ligou");
+Console.Writeline ("Mas eu não dou trela, eu não sou pela então não vou");
+
+Console.Readline ("");
+
+Console.Writeline ("Acha que eu caio pelo o estilo traiçoeira");
+Console.Writeline ("Vem com esse olhinho de quem pede uma besteira");
+Console.Writeline ("Tá de brincadeira, bitch cê nem tenta");
+Console.Writeline ("Porsche panamera, desce interesseira");
+
+Console.Readline ("");
+
+Console.Writeline ("Acha que eu caio pelo o estilo traiçoeira");
+Console.Writeline ("Vem com esse olhinho de quem pede uma besteira");
+Console.Writeline ("Tá de brincadeira, bitch cê nem tenta");
+Console.Writeline ("Porsche panamera, desce interesseira");
+
+Console.Readline ("");
+
+Console.Writeline ("Tô com-to com medo de me apaixonar yeah");
+Console.Writeline ("Tô só esperando cê decepcionar");
+Console.Writeline ("É tudo sempre o mesmo");
+Console.Writeline ("E eu não vou pensar");
+Console.Writeline ("Porque se eu pensar");
+Console.Writeline ("Capaz d'eu voltar ye");
+
+Console.Writeline ("Tô com-tô com medo de me apaixonar yeah");
+Console.Writeline ("Tô só esperando cê decepcionar");
+Console.Writeline ("É tudo sempre o mesmo");
+Console.Writeline ("E eu não vou pensar");
+Console.Writeline ("Porque se eu pensar");
+Console.Writeline ("Capaz d'eu voltar ye");
+Console.Writeline ("Diz que tá interessada, para");
+Console.Writeline ("Bitch vê se para, para");
+Console.Writeline ("Cola em outra cara, para");
+Console.Writeline ("Como não repara, se afasta");
+Console.Writeline ("Bitch eu te falo não quero me envolver");
+Console.Writeline ("Olha só pro lado não existe eu e você");
+
+Console.Readline ("");
+
+Console.Writeline ("Diz que tá interessada, para");
+Console.Writeline ("Bitch vê se para, para");
+Console.Writeline ("Cola em outra cara, para");
+Console.Writeline ("Como não repara, se afasta");
+Console.Writeline ("Bitch eu te falo não quero me envolver");
+Console.Writeline ("Olha só pro lado não existe eu e você");
+
+Console.Readline ("");
+
+Console.Writeline ("Acha que eu caio pelo o estilo traiçoeira");
+Console.Writeline ("Vem com esse olhinho de quem pede uma besteira");
+Console.Writeline ("Tá de brincadeira, bitch cê nem tenta");
+Console.Writeline ("Porsche panamera, desce interesseira");
+Console.Writeline ("Acha que eu caio pelo o estilo traiçoeira");
+Console.Writeline ("Vem com esse olhinho de quem pede uma besteira");
+Console.Writeline ("Tá de brincadeira, bitch cê nem tenta");
+Console.Writeline ("Porsche panamera, desce interesseira");
+Console.Writeline ("Tô com-tô com medo de me apaixonar yeah");
+Console.Writeline ("Tô só esperando cê decepcionar");
+Console.Writeline ("É tudo sempre o mesmo");
+Console.Writeline ("E eu não vou pensar");
+Console.Writeline ("Porque se eu pensar");
+Console.Writeline ("Capaz d'eu voltar ye");
+Console.Writeline ("Tô com-tô com medo de me apaixonar yeah");
+Console.Writeline ("Tô só esperando cê decepcionar");
+Console.Writeline ("É tudo sempre o mesmo");
+Console.Writeline ("E eu não vou pensar");
+Console.Writeline ("Porque se eu pensar");
+Console.Writeline ("Capaz d'eu voltar ye");
